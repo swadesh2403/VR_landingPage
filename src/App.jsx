594af1562c47a6b3ea3dc80a84d084d98389ba17
swadesh2403 <sarkarswadesh007@gmail.com>
2024-12-28 08:39:32 +0530

    complte project

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,25 @@
-import React from 'react'
+import Navbar from "./components/Navbar";
+import HeroSection from "./components/HeroSection";
+import FeatureSection from "./components/FeatureSection";
+import Workflow from "./components/Workflow";
+import Footer from "./components/Footer";
+import Pricing from "./components/Pricing";
+import Testimonials from "./components/Testimonials";
 
 const App = () => {
   return (
-    <div>
-      <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
-    </div>
-  )
-}
+    <>
+      <Navbar />
+      <div className="max-w-7xl mx-auto pt-20 px-6">
+        <HeroSection />
+        <FeatureSection />
+        <Workflow />
+        <Pricing />
+        <Testimonials />
+        <Footer />
+      </div>
+    </>
+  );
+};
 
-export default App
+export default App;
